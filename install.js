4a151d68f9b1ee050d9488083ba11347fbdb359c
Subhajit Sahu <wolfram77@gmail.com>
2018-11-09 08:40:51 +0530

    setup(): use setup-youtubeuploader

--- install.js
+++ install.js
@@ -1,31 +1,16 @@
-const download = require('download');
 const cp = require('child_process');
 
 
-// Global variables.
-const RELEASE = '18.14';
-const URLPREFIX = 'https://github.com/porjo/youtubeuploader/releases/download';
-const ARCH = {
-  arm: 'armv7',
-  x64: 'amd64'
-};
-const PLATFORM = {
-  darwin: 'mac',
-  win32: 'windows'
+// Run a command, return true if success.
+function cpRun(txt) {
+  try { cp.execSync(txt, {stdio: []}); }
+  catch(e) { return false; }
+  return true;
 };
 
-
-// Get download URL.
-function url() {
-  var arch = ARCH[process.arch]||process.arch;
-  var platform = PLATFORM[process.platform]||'linux';
-  var ext = platform!=='linux'? '.zip':'.gz';
-  return `${URLPREFIX}/${RELEASE}/youtubeuploader_${platform}_${arch}${ext}`;
+// Setup "youtubeuploader".
+function setup() {
+  if(cpRun('youtubeuploader -v')) return;
+  cpRun('npm install -g setup-youtubeuploader');
 };
-
-// Main installer.
-async function main() {
-  await download(url(), __dirname, {extract: true});
-  // cp.execSync('rm -rf node_nodules');
-};
-main();
+setup();
